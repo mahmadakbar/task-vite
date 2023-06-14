e77af5e6abf4b7b83d6a6d20d2b0eef6c1b899d8
mahmadakbar <mahmadakbar@live.com>
2023-06-14 16:47:56 +0700

    day 2

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-stone-400 py-5 px-10 flex">
+      <h1 className="font-semibold flex-1">The Dashboard</h1>
+
+      <div className="rounded-full w-8 h-8 border bg-white"></div>
+    </header>
+  );
+}
---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,12 @@
+export default function Menu() {
+  return (
+    <section className="bg-stone-300 max-w-xs h-screen">
+      <ul>
+        <li className="py-3 pl-3 bg-stone-200">Home</li>
+        <li className="py-3 pl-3">Statistic</li>
+        <li className="py-3 pl-3">User</li>
+        <li className="py-3 pl-3">CMS</li>
+      </ul>
+    </section>
+  );
+}
